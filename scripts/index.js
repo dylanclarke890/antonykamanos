0c9ba85cb3fed715853b6a6dc604194c535aae6b
dylanclarke890 <dylanclarke890@gmail.com>
2022-11-21 09:30:42 +0000

    replaced scroll to id function

--- scripts/index.js
+++ scripts/index.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
   $(".scroll-link").on("click", function (event) {
     event.preventDefault();
     var sectionID = $(this).attr("data-id");
-    scrollToID("#" + sectionID, 750);
+    scrollTo(sectionID);
   });
   // scroll to top action
   $(".scroll-top").on("click", function (event) {
@@ -37,14 +37,12 @@ $(document).ready(function () {
   });
 });
 
-// scroll function
-function scrollToID(id, speed) {
-  var offSet = 0;
-  var targetOffset = $(id).offset().top - offSet;
-  var mainNav = $("#main-nav");
-  $("html,body").animate({ scrollTop: targetOffset }, speed);
-  if (mainNav.hasClass("open")) {
-    mainNav.css("height", "1px").removeClass("in").addClass("collapse");
-    mainNav.removeClass("open");
-  }
+function scrollTo(id) {
+  console.log("HIT");
+  const target = document.getElementById(id);
+  const y = target.getBoundingClientRect().top + window.scrollY;
+  window.scroll({
+    top: y,
+    behavior: "smooth",
+  });
 }
