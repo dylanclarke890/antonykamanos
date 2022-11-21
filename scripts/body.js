a741014f765c026db086778f7c949a3ae12be0a3
dylanclarke890 <dylanclarke890@gmail.com>
2022-11-21 09:17:43 +0000

    updated image names and removed redundant images

--- scripts/body.js
+++ scripts/body.js
@@ -7,13 +7,13 @@ jQuery(function ($) {
         $("#menu").hide();
       }
       $("#menubutt").show();
-      $("#logo").attr("src", "images/logo_small.png");
+      $("#logo").attr("src", "images/logo-mobile.png");
     } else if ($(this).width() >= 768) {
       if (!open) {
         $("#menu").show();
       }
       $("#menubutt").hide();
-      $("#logo").attr("src", "images/logo_big_def.png");
+      $("#logo").attr("src", "images/logo-web.png");
     }
   }
 
