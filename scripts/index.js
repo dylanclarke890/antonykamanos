1f8e53793945431c869633d9f346f127b5a8007b
dylanclarke890 <dylanclarke890@gmail.com>
2022-11-24 11:44:37 +0000

    moved shared code to utils

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,5 @@
 document.addEventListener("DOMContentLoaded", addEvents);
 
-function scrollToElement(id) {
-  if (!id) return;
-  const target = document.getElementById(id);
-  const y = target.getBoundingClientRect().top + window.scrollY;
-  scrollTo(y);
-}
-
-function scrollTo(y) {
-  window.scroll({
-    top: y,
-    behavior: "smooth",
-  });
-}
-
-function isWithinSection(section, sibling, yPos) {
-  const y = yPos ?? window.scrollY;
-  return y > section.offsetTop && y < sibling.offsetTop;
-}
-
-function isWithinAnySection(sections, yPos) {
-  const y = yPos ?? window.scrollY;
-  for (let current of sections) {
-    const next = current.nextElementSibling;
-    if ((!next || next.localName !== "div") && current.offsetTop <= y) return true; // within final section
-    if (isWithinSection(current, next, y)) return true;
-  }
-  return false;
-}
-
 function addEvents() {
   const scrollLinks = document.getElementsByClassName("scroll-link");
   for (let link of scrollLinks)
